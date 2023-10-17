f6993ca3f248726a3a36a4768d7be9add51517df
Kristjan Lääne <xonproject@gmail.com>
2023-10-17 21:45:00 +0300

    searchbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import { Link } from 'react-router-dom'
-import './Navbar.css'
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Searchbar } from "./Searchbar";
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <nav>
-        <Link to='/' className='brand'>
+        <Link to="/" className="brand">
           <h1>Cooking ninja</h1>
         </Link>
-        <Link to='/create'>
+        <Searchbar />
+        <Link to="/create">
           <h1>Create recipe</h1>
         </Link>
       </nav>
     </div>
-  )
-}
+  );
+};
