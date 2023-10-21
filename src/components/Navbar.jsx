3559d68c5a9565e36080b999d82a4e0a597e51b8
Kristjan Lääne <xonproject@gmail.com>
2023-10-21 12:27:08 +0300

    accessing context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { Searchbar } from "./Searchbar";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 export const Navbar = () => {
+  const { color } = useContext(ThemeContext);
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking ninja</h1>
